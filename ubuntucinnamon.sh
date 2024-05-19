e09e2b0bc55d8a70a30cab2f60ec88a059151982
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2024-05-20 00:57:29 +0300

    Update ubuntucinnamon.sh

--- ubuntucinnamon.sh
+++ ubuntucinnamon.sh
@@ -1,13 +1,23 @@
 #Version 1.0
 echo "Sam's epic ubuntu cinnamon install script"
-echo "Recommended Driver:470"
+echo "Recommended Nvidia Driver:470"
 sleep 5 
 sudo apt install steam
-#Install wine start
+sudo apt install wget
+sudo apt install git
+sudo apt install adb
+
+#Install Wine start
 sudo dpkg --add-architecture i386
 sudo mkdir -pm755 /etc/apt/keyrings
 sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
 sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/mantic/winehq-mantic.sources
 sudo apt update
 sudo apt install --install-recommends winehq-stable
-#Install wine done
+#Install Wine finish
+
+#Install fastfetch
+wget https://github.com/fastfetch-cli/fastfetch/releases/download/2.12.0/fastfetch-linux-amd64.deb -O /home/$USER/Downloads/fastfetch.deb
+sudo dpkg -i /home/$USER/Downloads/fastfetch.deb
+sudo rm /home/$USER/Downloads/fastfetch.deb
+
