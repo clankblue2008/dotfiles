4ef61d2d4af00d4309743a97351d6e4628b914c3
Andrew <68531196+ant-7802@users.noreply.github.com>
2024-05-21 20:18:43 -0400

    Fix a couple issues with the script

--- ubuntucinnamon.sh
+++ ubuntucinnamon.sh
@@ -2,11 +2,7 @@
 echo "Sam's epic ubuntu cinnamon install script"
 echo "Recommended Nvidia Driver:470"
 sleep 5 
-sudo apt install steam
-sudo apt install wget
-sudo apt install git
-sudo apt install adb
-sudo apt install cava
+sudo apt install steam wget git adb cava -y
 
 #Install Wine start
 sudo dpkg --add-architecture i386
@@ -14,13 +10,13 @@ sudo mkdir -pm755 /etc/apt/keyrings
 sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
 sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/mantic/winehq-mantic.sources
 sudo apt update
-sudo apt install --install-recommends winehq-stable
+sudo apt install --install-recommends winehq-staging -y
 #Install Wine finish
 
 #Install fastfetch
-wget https://github.com/fastfetch-cli/fastfetch/releases/download/2.12.0/fastfetch-linux-amd64.deb -O /home/$USER/Downloads/fastfetch.deb
-sudo dpkg -i /home/$USER/Downloads/fastfetch.deb
+wget https://github.com/fastfetch-cli/fastfetch/releases/download/2.12.0/fastfetch-linux-amd64.deb -O /tmp/fastfetch.deb
+sudo apt install /tmp/fastfetch.deb -y
 sudo rm /home/$USER/Downloads/fastfetch.deb
 
 #AppImages
-sudo curl https://raw.githubusercontent.com/srevinsaju/zap/main/install.sh | sudo bash -s
+#sudo curl https://raw.githubusercontent.com/srevinsaju/zap/main/install.sh | sudo bash -s (Zap is currently broken)
